bd6f7a689304d7ee01d21326c15305ed44d1961f
Thiam Hock Ng <nthock@gmail.com>
2018-07-14 15:55:37 +0800

    Refactor factories

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule BackendPhoenix.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "web", "test/support", "test/factories"]
   defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
---
+++ test/factories/user_factory.ex
@@ -0,0 +1,18 @@
+defmodule BackendPhoenix.UserFactory do
+  alias BackendPhoenix.Accounts.User
+
+  defmacro __using__(_opts) do
+    quote do
+      def user_factory do
+        %User{
+          name: "Jimmy",
+          email: "jimmy@example.com",
+          designation: "Founder",
+          password: "password",
+          super_admin: true,
+          admin: true
+        }
+      end
+    end
+  end
+end
